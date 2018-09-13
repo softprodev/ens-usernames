4798b3d8ec6aea4ad45b095fb991d22101736ef6
SoftProDev <softprodev15@gmail.com>
2018-09-13 11:56:49 -0400

    deploy updated username registry on ropsten

--- config/contracts.js
+++ config/contracts.js
@@ -110,7 +110,7 @@ module.exports = {
         "address": "0x58E01078d14142E0370526dFdAE44E4f508c844B"
       },
       "UsernameRegistrar": {
-        "address": "0x2e1239715c5d7A84002328D2D8B8307fd509937c"
+        "address": "0x11d9F481effd20D76cEE832559bd9Aca25405841"
       }
     }
   },
